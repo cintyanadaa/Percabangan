6b0cc26fbbb4b814e9c53392b66ee006c45bf8a5
cintyanadaa <cintyanadaa17@gmail.com>
2020-11-10 18:28:13 +0700

    publish file index

---
+++ Percabangan/src/com/dicoding/javafundamental/percabangan/IfThen.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.percabangan;
+
+/**
+ *
+ * @author Sindy
+ */
+public class IfThen {
+     public static void main(String[] args) {
+        boolean isOn = true;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        }
+    }
+        }
+    
+
---
+++ Percabangan/src/com/dicoding/javafundamental/percabangan/IfThenElse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.percabangan;
+
+/**
+ *
+ * @author Sindy
+ */
+public class IfThenElse {
+    public static void main(String[] args) {
+        boolean isOn = false;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        } else {
+            System.out.println("Kondisi tidak terpenuhi...");
+        }
+    }
+}
+    
+
